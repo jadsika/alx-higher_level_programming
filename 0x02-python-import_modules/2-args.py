abd46fa39946903720be0824114591eec8b63790
jadsika <jadgogovi@gmail.com>
2023-12-06 20:25:33 -0800

    task 2

---
+++ 0x02-python-import_modules/2-args.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    """Print the numbewr of list of argument"""
+    from sys import argv
+
+    num_args = len(argv) - 1
+
+    if num_args == 0:
+        print("0 arguments.")
+    elif num_args == 1:
+        print("1 argument:")
+    else:
+        print("{:d} arguments:".format(num_args))
+
+    for i in range(1, num_args + 1):
+        print("{:d}: {}".format(i, argv[i]))
