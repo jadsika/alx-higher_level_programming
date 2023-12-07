63b39b8a1d090616664eb4cb11f47b36d637cb56
jadsika <jadgogovi@gmail.com>
2023-12-06 18:37:44 -0800

    0-task

---
+++ 0x02-python-import_modules/add_0.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+def add(a,b):
+    """My addition function
+
+    Args:
+       a: first integer
+       b: second integer
+
+    Returns:
+       The retuen value. a + b
+    """
+    return(a+b)
